545fcf5983140b34823bdec39c18452d24752bfe
Bolin <972497936@qq.com>
2022-02-20 10:55:18 +0800

    Add a header

---
+++ Code/common.h
@@ -0,0 +1,11 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+/* global marco */
+
+
+/* common functions */
+
+void perror(int error_type, int line_num, char* msg);
+
+#endif
---
+++ Code/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc,char* argv[]){
+    /* command line arguments */
+    if(argc!=2){
+        fprintf(stderr,"usage: parser *.cmm\n");
+        exit(1);
+    }
+    FILE * fp = NULL;
+    fp = fopen(argv[1],"r");
+    if(!fp){
+        fprintf(stderr, "target \"%s\" not exist\n");
+        exit(1);
+    }
+    
+    
+    
+    fclose(fp);
+    fp = NULL;
+    return 0;
+}
+
