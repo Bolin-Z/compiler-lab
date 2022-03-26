f2611b3a057eb63224884d541592d6def988635c
BolinZ <972497936@qq.com>
2022-03-26 21:38:16 +0800

    Add Data Structure for SymTable and TypeSys

--- Code/symtab.h
+++ Code/symtab.h
@@ -1,7 +1,71 @@
 #ifndef __SYMTAB_H__
 #define __SYMTAB_H__
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include"typesys.h"
 
+# define SYMSTACKSIZE 256
+# define HASHTABLESIZE 256
+# define SCOPESTACKSIZE 16
 
+extern SymbolTable symtab;
+
+typedef struct Symbol{
+    /* fields related to Symbol table */
+    char* _id;
+    size_t pre;
+    size_t nxt;
+    /* fields contains infomation of the symbol */
+    Attribute _attribute;
+} Symbol;
+
+typedef struct SymbolStack{
+    Symbol * _stack;
+    size_t cur;
+    size_t _stacksize;
+} SymbolStack;
+
+typedef struct SymbolHashTable{
+    size_t HashList[HASHTABLESIZE];
+} SymbolHashTable;
+
+typedef struct Scope{
+    char * ScopeName;
+    size_t Scopeidx;
+} Scope;
+
+typedef struct ScopeStack{
+    Scope * _stack;
+    size_t cur;
+    size_t _stacksize;
+} ScopeStack;
+
+typedef struct SymbolTable{
+    SymbolHashTable _hashtable;
+    SymbolStack _symstack;
+    ScopeStack _scopestack;
+} SymbolTable;
+
+typedef struct Attribute{
+    enum {ERROR,VARIABLE,FUNCTION,TYPENAME} IdClass;
+    TypeDescriptor * _idtype;
+    // type
+    union{
+        int dummy;
+        float dummy2;
+    };
+} Attribute;
+
+/* API */
+int initSymbolTable();
+
+size_t OpenScope(char* ScopeName);
+void CloseScope();
+Scope * CurScope();
+
+Symbol * Insert(char* name);
+Symbol * LookUp(char* name, bool curscope);
 
 #endif
--- Code/typesys.h
+++ Code/typesys.h
@@ -1,7 +1,22 @@
 #ifndef __TYPESYS_H__
 #define __TYPESYS_H__
 
+typedef struct TypeDescriptor{
+    enum {BASIC, ARRAY, STRUCTURE, ERROR} typeform;
+    union{
+        /* typeform == Basic */
+        enum {INT,FLOAT} basic;
+        /* typeform == array */
+        struct {TypeDescriptor * elem; size_t size;} _array;
+        /* typeform == structure */
+        FieldList * _structure;
+    };
+} TypeDescriptor;
 
-
+typedef struct FieldList{
+    char * fieldname;
+    TypeDescriptor * fieldtype;
+    FieldList * nextfield;
+} FieldList;
 
 #endif
