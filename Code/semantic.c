910345c0a527f40a87b41fd98fe6dc2ec51a40cf
BolinZ <972497936@qq.com>
2022-03-31 23:17:51 +0800

    Working on SemanticAnalysisExp()

--- Code/semantic.c
+++ Code/semantic.c
@@ -7,7 +7,7 @@ SA(void, ExtDef);
 SA(TypeDescriptor*, Specifier);
 SA(void, DefList, bool fields);
 SA(void, Def, bool field);
-SA(TypeDescriptor*, Exp);
+SA(TypeDescriptor*, Exp, bool LeftHand);
 
 void SemanticAnalysis(const struct CST_node* root){
     SymbolTable * symtable = CreatSymbolTable();
@@ -165,11 +165,11 @@ SA(void, Def, bool field){
         newsymboal->attribute.IdType = pretype;
         if(curDec->child_cnt == 3){
             /* VarDec ASSIGNOP Exp */
-            TypeDescriptor * exptype = SemanticAnalysisExp(curDec->child_list[2],symtab);
+            TypeDescriptor * exptype = SemanticAnalysisExp(curDec->child_list[2],symtab,false);
             if(field) ReportSemanticError(0,0,"Field can not be initialized");
             else{
                 if(!IsEqualType(newsymboal->attribute.IdType,exptype))
-                    ReportSemanticError(0,0,"Unmatch Exp type");
+                    ReportSemanticError(0,0,"Type mismatched for assignment");
             }
         }
         if(curDecList->child_cnt == 1) break;
@@ -181,6 +181,141 @@ SA(void, Def, bool field){
     Return a pointer to a TypeDescriptor that describes Exp.
      No new TypeDescriptor will be created.
 */
-SA(TypeDescriptor*, Exp){
-
+SA(TypeDescriptor*, Exp, bool LeftHand){
+    int Production;
+    switch(n->child_cnt){
+        case 1 :
+            switch(get_symtype(n->child_list[0]->compact_type)){
+                case SYM(ID) : Production = 16; break;
+                case SYM(INT) : Production = 17; break;
+                case SYM(FLOAT) : Production = 18; break;
+                default : Production = 0; break;
+            }
+            break;
+        case 2 :
+            switch(get_symtype(n->child_list[0]->compact_type)){
+                case SYM(MINUS) : Production = 10; break;
+                case SYM(NOT) : Production = 11; break;
+                default : Production = 0; break;
+            }
+            break;
+        case 3 :
+            switch(get_symtype(n->child_list[1]->compact_type)){
+                case SYM(ASSIGNOP) : Production = 1; break;
+                case SYM(AND) : Production = 2; break;
+                case SYM(OR) : Production = 3; break;
+                case SYM(RELOP) : Production = 4; break;
+                case SYM(PLUS) : Production = 5; break;
+                case SYM(MINUS) : Production = 6; break;
+                case SYM(STAR) : Production = 7; break;
+                case SYM(DIV) : Production = 8; break;
+                case SYM(Exp) : Production = 9; break;
+                case SYM(LP) : Production = 13; break;
+                case SYM(DOT) : Production = 15; break;
+                default : Production = 0; break;
+            }
+            break;
+        case 4 :
+            switch(get_symtype(n->child_list[1]->compact_type)){
+                case SYM(LP) : Production = 12; break;
+                case SYM(LB) : Production = 14; break;
+                default : Production = 0; break;
+            }
+            break;
+        default : Production = 0; break;
+    }
+    switch(Production){
+        case 1 : /* Exp ASSIGNOP Exp */
+            {
+                TypeDescriptor * lexp = SemanticAnalysisExp(n->child_list[0], symtab, true);
+                TypeDescriptor * rexp = SemanticAnalysisExp(n->child_list[2], symtab, LeftHand);
+                if(IsEqualType(lexp,BasicError()) || IsEqualType(rexp,BasicError())){
+                    /* lexp or rexp contains error */
+                    return BasicError();
+                }else{
+                    if(!IsEqualType(lexp,rexp)){
+                        ReportSemanticError(0,0,"Type mismatched for assignment");
+                        return BasicError();
+                    }
+                    return lexp;
+                }
+            }
+        case 2 : /* Exp AND Exp */
+        case 3 : /* Exp OR Exp */
+        case 4 : /* Exp RELOP Exp */
+            {
+                if(LeftHand){
+                    ReportSemanticError(0,0,"The left-hand side of an assignment must be left value");
+                    return BasicError();
+                }else{
+                    TypeDescriptor * lexp = SemanticAnalysisExp(n->child_list[0],symtab,false);
+                    TypeDescriptor * rexp = SemanticAnalysisExp(n->child_list[2],symtab,false);
+                    if(IsEqualType(lexp,BasicError()) || IsEqualType(rexp,BasicError())){
+                        /* lexp or rexp contains error */
+                        return BasicError();
+                    }else{
+                        bool lint = IsEqualType(lexp,BasicInt());
+                        bool rint = IsEqualType(rexp,BasicInt());
+                        if(!lint) ReportSemanticError(0,0,"Expected int value in lexp");
+                        if(!rint) ReportSemanticError(0,0,"Expected int value in rexp");
+                        if(lint && rint) return BasicInt();
+                        else return BasicError();
+                    }
+                }
+            }
+        case 5 : /* Exp PLUS Exp */
+        case 6 : /* Exp MINUS Exp */
+        case 7 : /* Exp STAR Exp */
+        case 8 : /* Exp DIV Exp */
+            {
+                if(LeftHand){
+                    ReportSemanticError(0,0,"The left-hand side of assignment must be left value");
+                    return BasicError();
+                }else{
+                    TypeDescriptor * lexp = SemanticAnalysisExp(n->child_list[0],symtab,false);
+                    TypeDescriptor * rexp = SemanticAnalysisExp(n->child_list[2],symtab,false);
+                    if(IsEqualType(lexp,BasicError()) || IsEqualType(rexp,BasicError())){
+                        return BasicError();
+                    }else{
+                        if(IsEqualType(lexp,rexp)){
+                            if(IsEqualType(lexp,BasicInt()) || IsEqualType(lexp,BasicFloat()))
+                                return lexp;
+                            else{
+                                ReportSemanticError(0,0,"Only int and float types can do arithmetic operation");
+                                return BasicError();
+                            }                            
+                        }
+                        else{
+                            ReportSemanticError(0,0,"Type mismatched for operands");
+                            return BasicError();
+                        }
+                    }
+                }
+            }
+        case 9 : /* LP Exp RP */
+            return SemanticAnalysisExp(n->child_list[1],symtab,LeftHand);
+        case 10 : /* MINUS Exp */
+            {
+                if(LeftHand){
+                    ReportSemanticError(0,0,"The left-hand side of assignment must be left value");
+                    return BasicError();
+                }else{
+                    TypeDescriptor * exp = SemanticAnalysisExp(n->child_list[1],symtab,false);
+                    if(IsEqualType(exp,BasicInt()) || IsEqualType(exp,BasicFloat()))
+                        return exp;
+                    else{
+                        ReportSemanticError(0,0,"Only int and float types can do arithmetic operation");
+                        return BasicError();
+                    }
+                }
+            }
+        case 11 : /* NOT Exp */
+            {
+                if(LeftHand){
+                    ReportSemanticError(0,0,"The left-hand side of assignment must be left value");
+                }
+            }
+        default : /* error */
+            return BasicError();
+    }
 }
