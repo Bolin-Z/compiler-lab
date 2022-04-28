4aa6a00bd64faa3f686b1f8d17197d8784ad72fa
BolinZ <972497936@qq.com>
2022-04-28 22:13:22 +0800

    <main.c>: Prepare for stage 3

--- Code/main.c
+++ Code/main.c
@@ -5,6 +5,7 @@
 #include"error.h"
 #include"cst.h"
 #include"semantic.h"
+#include"ir.h"
 
 extern void yyrestart(FILE*);
 extern bool has_error;
@@ -12,12 +13,11 @@ struct CST_node * cst_root;
 
 int main(int argc,char* argv[]){
     /* command line arguments */
-    if(argc!=2){
-        fprintf(ERROR_MSG_2,"Usage: %s *.cmm\n",argv[0]);
+    if(argc!=3){
+        fprintf(ERROR_MSG_2,"Usage: %s *.cmm *.ir\n",argv[0]);
         exit(1);
     }
-    FILE * fp = NULL;
-    fp = fopen(argv[1],"r");
+    FILE * fp = fopen(argv[1],"r");
     if(!fp){
         fprintf(ERROR_MSG_2, "target \"%s\" not exist\n",argv[1]);
         exit(1);
@@ -27,7 +27,7 @@ int main(int argc,char* argv[]){
     cst_root = NULL;
     yyparse();
     fclose(fp);
-    fp = NULL;
+
 /* Project 1  
     if(has_error == false){
         if(cst_root != NULL){
@@ -38,11 +38,23 @@ int main(int argc,char* argv[]){
 */
 
 /* Project 2 */
+    irSystem * irSys = NULL;
     if(!has_error){
+        irSys = creatIrSystem();
         SemanticAnalysis(cst_root);
         destory_tree(cst_root);
     }
 
+/* Project 3*/
+    if(!has_error){
+        // optimize()
+        FILE * outputIrCode = fopen(argv[2],"w+");
+        if(outputIrCode){
+            fprintfIrCode(outputIrCode,irSys);
+        }
+    }
+    destoryIrSystem(irSys);
+    
     cst_root = NULL;
     return 0;
 }
